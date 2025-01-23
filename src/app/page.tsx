4e63550ed810bdf6a19e899c6bb8a7866a220888
Hardy-Hao <ghost7021.hao@gmail.com>
2025-01-23 11:07:42 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className={styles.page}>
+      <h1>部署到 Vercel 的 Next.js 專案</h1>
       <Link href='/client-fetching-data'>
         <h1>client-fetching-data</h1>
       </Link>
